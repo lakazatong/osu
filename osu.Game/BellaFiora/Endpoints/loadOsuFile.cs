41a7b1456efe15b962187de5e16b57a0eeed2bbf
lakazatong <Bo_wood@outlook.fr>
2025-04-26 01:54:25 +0200

    restore snapping commented out

--- osu.Game/BellaFiora/Endpoints/loadOsuFile.cs
+++ osu.Game/BellaFiora/Endpoints/loadOsuFile.cs
@@ -10,6 +10,9 @@ using osu.Game.IO;
 using osu.Game.Beatmaps;
 using osu.Game.Tests.Beatmaps;
 using osu.Game.Rulesets;
+using osu.Game.Rulesets.Objects;
+using osu.Game.Beatmaps.ControlPoints;
+using System.Linq;
 
 namespace osu.Game.BellaFiora.Endpoints
 {
@@ -29,6 +32,22 @@ namespace osu.Game.BellaFiora.Endpoints
             }
             return false;
         };
+        private double snapStartTime(double timeMs, double resolutionMs, double previousNoteTime)
+        {
+            double adjustedTime = timeMs - previousNoteTime;
+            return Math.Round(adjustedTime / resolutionMs) * resolutionMs + previousNoteTime;
+        }
+        private void tryRestoreSnapping(IBeatmap beatmap)
+        {
+            double previousNoteTime = (double)beatmap.HitObjects[0].StartTime;
+            foreach (HitObject o in beatmap.HitObjects.Skip(1).ToList())
+            {
+                TimingControlPoint cp = beatmap.ControlPointInfo.TimingPointAt(o.StartTime);
+                o.StartTime = snapStartTime((double)o.StartTime, cp.BeatLength / cp.TimeSignature.Numerator, previousNoteTime);
+                // previousNoteTime = o.StartTime;
+            }
+
+        }
         private void callback(string path, string? mode)
         {
             Server.UpdateThread.Post(_ =>
@@ -39,8 +58,10 @@ namespace osu.Game.BellaFiora.Endpoints
                     var beatmap = Decoder.GetDecoder<Beatmap>(reader).Decode(reader);
                     beatmap.BeatmapInfo.UpdateStatisticsFromBeatmap(beatmap);
 
-                    var wBeatmap = new TestWorkingBeatmap(beatmap);
-                    Server.RespondJSON(wBeatmap.GetPlayableBeatmap(mode == null || mode.Length == 0 ? beatmap.BeatmapInfo.Ruleset : new RulesetInfo { ShortName = mode, Available = true }));
+                    IBeatmap playableBeatmap = new TestWorkingBeatmap(beatmap).GetPlayableBeatmap(mode == null || mode.Length == 0 ? beatmap.BeatmapInfo.Ruleset : new RulesetInfo { ShortName = mode, Available = true });
+                    // tryRestoreSnapping(playableBeatmap);
+
+                    Server.RespondJSON(playableBeatmap);
                 }
             }, null);
         }
